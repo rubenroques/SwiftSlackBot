da1f400478033a14872ab195f7a8b868810288f2
Ruben Roques <rubenroques.nhk@gmail.com>
2016-02-12 23:35:00 +0000

    Initial Commit

---
+++ SwiftSlackBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSlackBot
+//
+//  Created by Ruben Roques on 12/02/16.
+//  Copyright © 2016 Ruben Roques. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
